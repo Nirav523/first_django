5f7f2e331d1f576e3f4f56a406ce129d7190dd43
Nirav523 <128112001+Nirav523@users.noreply.github.com>
2025-02-05 17:09:02 +0530

    Update views.py

--- videos/views.py
+++ videos/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from django.template import loader
 
 
+def home(request):
+    return HttpResponse("Welcome to the homepage!")
+
+
 def videos(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
